d33988c290b1a83d00e20bf7384adb4a3a078c95
Adi Bines <p0037082@ness-tech.co.il>
2024-06-18 12:53:26 +0300

    removing line

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="login-container"] div').filter({ hasText: 'Login' }).first().click();
   await page.locator('[data-test="login-credentials"]').click();
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
