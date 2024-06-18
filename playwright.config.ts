f7308e6c8bad18208ec1cc8af1bef7f9cde52f12
Adi Bines <p0037082@ness-tech.co.il>
2024-06-18 12:09:19 +0300

    adding basic project structur

---
+++ playwright.config.ts
@@ -0,0 +1,89 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+
+  /*fullyParallel: true, ריצה מקבלית נבטל */ 
+  /* Fail the build on CI if you accidentally left test.only in the source code. ONLY-יכול להרוס את הריצה הכוללת.אז פקודה זו מבטלת עניין זה*/
+  forbidOnly: !!process.env.CI,   /*מפיל את הריצה בענן במידה ויש טסט אונלי בקוד שלנו*/
+
+  /* Retry on CI only ירוץ פעמיים בענן.שזה לוקאלית פעם אחת ואם נכשל נכשל.בענן ינסה פעמיים למקרה שהראשון יכשל והשני לא*/
+  retries: process.env.CI ? 2 : 0,
+
+  /* Opt out of parallel tests on CI. להגדיר מספר דפדפנים שייפתחו מקביל לואקלית אפשר כמה במקביל ואפשר לנצל משאבים*/
+  workers: process.env.CI ? 1 : 2,
+
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. הגדרות כלליות*/
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. אתר שהוא הבסיס לשאר הבדיקות,ואז עליו להוסיף סיומות.זה שימושי לבדוק דפים כתובות נכונות וכו*/
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    // מידע שנאסף ונוכל לדעת מה קרה עם הטסט לעומק במקרה של כישלון
+    trace: 'on-first-retry',
+
+    //נוסיף-
+    screenshot:'on',
+    video: 'on-first-retry',
+    headless: process.env.CI ? true : false
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      //נשתמש בכרום את השאר ניתן לדרוס
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    /*{
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },*/
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+    //בדיקות קומפוננט של ריאקט.ניתן להרים את הסרבס המקומי של ריאקט למען הבדיקות
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
---
+++ tests/test-1.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="login-container"] div').filter({ hasText: 'Login' }).first().click();
+  await page.locator('[data-test="login-credentials"]').click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="secondary-header"]').click();
+  await page.getByText('1Sauce Labs Backpackcarry.').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('adi');
+  await page.locator('[data-test="firstName"]').press('Tab');
+  await page.locator('[data-test="lastName"]').fill('bines');
+  await page.locator('[data-test="lastName"]').press('Tab');
+  await page.locator('[data-test="postalCode"]').fill('121212');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="payment-info-label"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+  await page.locator('[data-test="login-container"] div').filter({ hasText: 'Login' }).first().click();
+  await page.getByText('Swag Labs').click();
+  await page.locator('[data-test="login-credentials"]').click();
+});
